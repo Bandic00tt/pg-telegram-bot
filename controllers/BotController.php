b4f1c428fb05a8fed726707af0454a8e09812c16
Oleg Serebryakov <olegserebryakoff@mail.ru>
2017-09-21 14:51:58 +0300

    fxs

---
+++ controllers/BotController.php
@@ -0,0 +1,13 @@
+<?php
+namespace app\controllers;
+
+use Yii;
+use yii\web\Controller;
+
+class BotController extends Controller
+{
+    public function actionIndex()
+    {
+        return 'bot index';
+    }        
+}
